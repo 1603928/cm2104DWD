0823ca8b9274b45dc42b6dbe29de65a82295fc03
CHARLIE MARSH (1603928) <1603928@rgu.ac.uk>
2018-04-18 11:55:51 +0100

    testing

--- lab18/server.js
+++ lab18/server.js
@@ -110,6 +110,7 @@ app.post('/dologin', function(req, res) {
   var pword = req.body.password;
 
   db.collection('people').findOne({"login.username":uname}, function(err, result) {
+    console.log("Got here")
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/login');return}
